cbd0289c36289129621751d1480e0a61a30550bc
Niti Nrw <niti.patel@flatironschool.com>
2017-03-05 18:21:39 -0500

    Update contact pic

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ import BackgroundImage from './BackgroundImage'
 export default function Contact(props) {
   return (
     <div>
-      <BackgroundImage link="/contact.jpg"/>
+      <div className="darken"><BackgroundImage link="/dark.png"/></div>
       <p className="title">Contact Me</p>
       <div className="white-block">
         <p className="body-header" id="bigger-text">Get in touch</p>
